867a23454811612126b21d9d1f68401c2f244811
Jesú Márquez <jesumarquez@gmail.com>
2016-08-02 14:49:35 -0300

    jspm. using ts-lib and coffee-lib.

--- src/entry.js
+++ src/entry.js
@@ -1,3 +1,6 @@
-var lib = require('./lib.js');
+//import lib from './es6-lib';
+//import lib from '../dist/coffee-lib';
+import lib from '../dist/ts-lib';
+
 console.log(lib.double(2));
 console.log(lib.addFive(2));
